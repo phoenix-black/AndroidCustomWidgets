402ed93b451537920dde26eec0c2221c8c69415f
Praba <prabalinux@gmail.com>
2017-10-17 00:07:11 +0530

    Initial commit

---
+++ app/src/main/java/com/blackphoenix/praba/androidcustomwidgets/ZToast.java
@@ -0,0 +1,22 @@
+package com.blackphoenix.praba.androidcustomwidgets;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Praba on 10/17/2017.
+ */
+public class ZToast {
+
+    public static void show(Context context, String message){
+        Toast.makeText(context,""+message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showShort(Context context, String message){
+        Toast.makeText(context,""+message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showLong(Context context, String message){
+        Toast.makeText(context,""+message,Toast.LENGTH_LONG).show();
+    }
+}
